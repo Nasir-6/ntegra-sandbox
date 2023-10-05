4648405b5e97e3ab2de8364f5d941c9471c3c80e
Mohammed Nasir <mohammednasir@Ntegras-MBP.cust.communityfibre.co.uk>
2023-10-05 13:16:47 +0100

    successfully send back all todos via faunadb, itty-router and cloudflare

--- todo-cloudflare-api/src/index.ts
+++ todo-cloudflare-api/src/index.ts
@@ -1,24 +1,63 @@
-import apiRouter from './router';
+import { Router, RouterType } from 'itty-router';
+import faunadb, { Collection, Documents, Get, Paginate, Ref, Query, Map, Index, Select } from 'faunadb';
 
-// Export a default object containing event handlers
+export interface Env {
+	FAUNADB_SECRET?: string;
+	router?: RouterType;
+}
+
+const corsHeaders = {
+	'Access-Control-Allow-Headers': '*', // What headers are allowed. * is wildcard. Instead of using '*', you can specify a list of specific headers that are allowed, such as: Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Authorization.
+	'Access-Control-Allow-Methods': 'POST', // Allowed methods. Others could be GET, PUT, DELETE etc.
+	'Access-Control-Allow-Origin': '*', // This is URLs that are allowed to access the server. * is the wildcard character meaning any URL can.
+};
+
+type Todo = {
+	id: number;
+	todo: string;
+	isDone: boolean;
+};
+
 export default {
-	// The fetch handler is invoked when this worker receives a HTTP(S) request
-	// and should return a Response (optionally wrapped in a Promise)
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
-		const url = new URL(request.url);
-
-		if (url.pathname.startsWith('/api/')) {
-			// You can also use more robust routing
-			return apiRouter.handle(request);
+	async fetch(request: Request, env: Env): Promise<Response> {
+		if (env.router === undefined) {
+			env.router = buildRouter(env);
 		}
 
-		return new Response(
-			`Try making requests to:
-      <ul>
-      <li><code><a href="/api/todos">/api/todos</a></code></li>
-	  </ul>`,
-			{ headers: { 'Content-Type': 'text/html' } }
-		);
+		return env.router.handle(request);
 	},
 };
+
+function buildRouter(env: Env): RouterType {
+	const router = Router();
+
+	const isLocalEnv = env.FAUNADB_SECRET;
+	const dbkey = isLocalEnv ? env.FAUNADB_SECRET : FAUNADB_SECRET;
+
+	const faunaClient = new faunadb.Client({
+		secret: dbkey,
+	});
+
+	router.get('/api/todos', async (request, env) => {
+		const dbQuery = await faunaClient.query(
+			Map(Paginate(Documents(Collection('todos'))), (ref) => {
+				return {
+					id: Select(['ref', 'id'], Get(ref)),
+					todo: Select(['data', 'todo'], Get(ref)),
+					isDone: Select(['data', 'isDone'], Get(ref)),
+				};
+			})
+		);
+		return new Response(JSON.stringify(dbQuery), {
+			headers: {
+				'Content-type': 'application/json',
+				...corsHeaders,
+			},
+		});
+	});
+
+	// 404 for everything else
+	router.all('*', () => new Response('Not Found.', { status: 404 }));
+
+	return router;
+}
